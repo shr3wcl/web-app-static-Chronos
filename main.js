72261f203273b9da23302c98cd127d3cf4c0b510
Minh Trí <minhtri192035@gmail.com>
2022-05-10 09:33:24 +0700

    add

--- main.js
+++ main.js
@@ -3,10 +3,11 @@ const modal = document.querySelector("#sign-pop-up-div");
 const formSignIn = document.querySelector("#form-sign-in");
 const formSignUp = document.querySelector("#form-sign-up");
 const signUpBtn = document.querySelector("#button-sign-up");
+const registernow = document.querySelector("#register-now");
 
 signin.addEventListener("click", function() {
     modal.classList.add("open");
-    // formSignIn.classList.add("open2");
+    formSignIn.classList.add("open2");
 });
 
 modal.addEventListener("click", function() {
@@ -24,6 +25,11 @@ formSignUp.addEventListener("click", function(event) {
 })
 
 signUpBtn.addEventListener("click", function() {
-    // formSignUp.classList.add("open2");
+    formSignUp.classList.add("open2");
     modal.classList.add("open");
+})
+
+registernow.addEventListener("click", function() {
+    formSignIn.classList.remove("open2");
+    formSignUp.classList.add("open2");
 })
