17e1332fc13e6a47e477c52876ebb70a38be506c
Minh Trí <minhtri192035@gmail.com>
2022-05-11 21:58:03 +0700

    temp

--- Layout.js
+++ Layout.js
@@ -6,6 +6,8 @@ const formStart = document.querySelector("#form-start");
 const signUpBtn = document.querySelector("#button-sign-up");
 const registernow = document.querySelector("#register-now");
 const startBtn = document.querySelector("#start-button");
+const RegBtn = document.querySelector(".register-btn");
+const signin2 = document.querySelector(".sign-in-btn");
 
 formSignIn.addEventListener("click", function(event) {
     event.stopPropagation();
@@ -34,14 +36,34 @@ modal.addEventListener("click", function() {
 signUpBtn.addEventListener("click", function() {
     formSignUp.classList.add("open2");
     modal.classList.add("open");
-})
+});
 
 registernow.addEventListener("click", function() {
     formSignIn.classList.remove("open2");
     formSignUp.classList.add("open2");
-})
+});
 
 startBtn.addEventListener("click", function() {
     modal.classList.add("open");
     formStart.classList.add("open3");
-})
+});
+
+RegBtn.addEventListener("click", function() {
+    formStart.classList.remove("open3");
+    formSignUp.classList.add("open2");
+});
+
+signin2.addEventListener("click", function() {
+    formStart.classList.remove("open3");
+    formSignIn.classList.add("open2");
+});
+
+signin2.addEventListener("mouseover", function() {
+    RegBtn.classList.add("animation-1")
+    signin2.classList.add("animation-2");
+});
+
+signin2.addEventListener("mouseout", function() {
+    RegBtn.classList.remove("animation-1");
+    signin2.classList.remove("animation-2");
+});
--- formSign.js
+++ formSign.js
@@ -5,13 +5,42 @@ const eye3 = document.querySelector(".sign-up-eye-retype");
 const eyeSlash2 = document.querySelector(".sign-up-eye-slash");
 const eyeSlash3 = document.querySelector(".sign-up-eye-slash-retype");
 
-// function showPass(eyeSlash, eye) {
-//     eye.
-//     eyeSlash.classlist.add(".no-display");
-
-// }
-
 eyeSlash1.addEventListener("click", function() {
-    eye1.classList.remove(".no-display");
-    eyeSlash1.classList.add(".no-display");
+    eye1.classList.add("open2");
+    eyeSlash1.classList.add("no-display");
+    document.getElementById("pw1").type = "text";
+});
+
+
+
+eye1.addEventListener("click", function() {
+    eyeSlash1.classList.remove("no-display");
+    eye1.classList.remove("open2");
+    document.getElementById("pw1").type = "password";
+});
+
+eyeSlash2.addEventListener("click", function() {
+    eye2.classList.add("open2");
+    eyeSlash2.classList.add("no-display");
+    document.getElementById("pw2").type = "text";
+});
+
+
+
+eye2.addEventListener("click", function() {
+    eyeSlash2.classList.remove("no-display");
+    eye2.classList.remove("open2");
+    document.getElementById("pw2").type = "password";
+});
+
+eyeSlash3.addEventListener("click", function() {
+    eye3.classList.add("open2");
+    eyeSlash3.classList.add("no-display");
+    document.getElementById("pw3").type = "text";
+});
+
+eye3.addEventListener("click", function() {
+    eyeSlash3.classList.remove("no-display");
+    eye3.classList.remove("open2");
+    document.getElementById("pw3").type = "password";
 });
