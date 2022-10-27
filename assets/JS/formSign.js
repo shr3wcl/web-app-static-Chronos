89f9a81c7375b09225b6b7210158049c4f10d15b
Bin-08-01 <minhtri192035@gmail.com>
2022-10-27 14:25:56 +0700

    🚀Commit

--- assets/JS/formSign.js
+++ assets/JS/formSign.js
@@ -5,7 +5,17 @@ const eye3 = document.querySelector(".sign-up-eye-retype");
 const eyeSlash2 = document.querySelector(".sign-up-eye-slash");
 const eyeSlash3 = document.querySelector(".sign-up-eye-slash-retype");
 
-eyeSlash1.addEventListener("click", function() {
+function SignInAction() {
+    const UserName = document.getElementById("sign-in__username").value;
+    const Password = document.getElementById("pw1").value;
+    if (UserName == "admin" && Password == "admin") {
+        location.assign("./view/chronos/chronos.html");
+    } else {
+        alert("Error");
+    }
+}
+
+eyeSlash1.addEventListener("click", function () {
     eye1.classList.add("open2");
     eyeSlash1.classList.add("no-display");
     document.getElementById("pw1").type = "text";
@@ -13,13 +23,13 @@ eyeSlash1.addEventListener("click", function() {
 
 
 
-eye1.addEventListener("click", function() {
+eye1.addEventListener("click", function () {
     eyeSlash1.classList.remove("no-display");
     eye1.classList.remove("open2");
     document.getElementById("pw1").type = "password";
 });
 
-eyeSlash2.addEventListener("click", function() {
+eyeSlash2.addEventListener("click", function () {
     eye2.classList.add("open2");
     eyeSlash2.classList.add("no-display");
     document.getElementById("pw2").type = "text";
@@ -27,19 +37,19 @@ eyeSlash2.addEventListener("click", function() {
 
 
 
-eye2.addEventListener("click", function() {
+eye2.addEventListener("click", function () {
     eyeSlash2.classList.remove("no-display");
     eye2.classList.remove("open2");
     document.getElementById("pw2").type = "password";
 });
 
-eyeSlash3.addEventListener("click", function() {
+eyeSlash3.addEventListener("click", function () {
     eye3.classList.add("open2");
     eyeSlash3.classList.add("no-display");
     document.getElementById("pw3").type = "text";
 });
 
-eye3.addEventListener("click", function() {
+eye3.addEventListener("click", function () {
     eyeSlash3.classList.remove("no-display");
     eye3.classList.remove("open2");
     document.getElementById("pw3").type = "password";
